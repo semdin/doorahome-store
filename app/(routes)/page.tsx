19f4fecee8f4d953f4a7bf8fe2a82a26e8d2c19b
semdin <mehmetsemdin.aktay@std.yeditepe.edu.tr>
2023-08-25 16:06:39 +0300

    environment-setup-and-featured-prod-cont

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,25 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container"
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({isFeatured:true});
+    const billboard = await getBillboard("20671e17-b7c7-4805-9d46-e806eaf4afcc");
+
     return (
-        <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products}/>
+                </div>
+            </div>
+        </Container>
     );
 }
 
